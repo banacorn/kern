81a24c360b5058bebcc7d0ceec557373e82f347e
Elliot Lai <ellliotlai@gmail.com>
2011-09-10 03:36:11 +0800

    rebuild

--- dev/scripts/script.coffee
+++ dev/scripts/script.coffee
@@ -1,2 +1,43 @@
 $ ->
-    console.log 'hello'
+    nav.init()
+    
+nav = (->
+
+    detectScroll = ->
+        headers = ($(header).position().top for header in $('h2'))
+        arr = (i for header, i in headers when $(window).scrollTop() >= header)
+        curr = if arr.length is 0 then 0 else arr.length - 1
+        _changeTag(curr)
+        console.log curr
+        
+    bindScroll = ->
+        $(window).scroll detectScroll
+    unbindScroll = ->
+        $(window).unbind 'scroll'
+
+    _changeTag = (index) ->
+        $('nav li.selected').removeClass 'selected'
+        $('nav li').eq(index).addClass 'selected'
+
+    return (
+        init: ->
+            that = @
+            $('nav li a').click(->
+                index = $(this).index('nav li a')
+                that.changeTag index
+                that.jump index
+            )
+                
+            $(window).scroll detectScroll
+            
+        changeTag: (index) -> _changeTag index
+            
+        jump: (index) ->
+            unbindScroll()
+            nextOffset = $('h2').eq(index).position().top
+            $('body').animate (
+                scrollTop: nextOffset
+            ), 300, bindScroll
+    
+    )
+)()
--- script.js
+++ script.js
@@ -1,5 +1,70 @@
 (function() {
+  var nav;
   $(function() {
-    return console.log('hello');
+    return nav.init();
   });
+  nav = (function() {
+    var bindScroll, detectScroll, unbindScroll, _changeTag;
+    detectScroll = function() {
+      var arr, curr, header, headers, i;
+      headers = (function() {
+        var _i, _len, _ref, _results;
+        _ref = $('h2');
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          header = _ref[_i];
+          _results.push($(header).position().top);
+        }
+        return _results;
+      })();
+      arr = (function() {
+        var _len, _results;
+        _results = [];
+        for (i = 0, _len = headers.length; i < _len; i++) {
+          header = headers[i];
+          if ($(window).scrollTop() >= header) {
+            _results.push(i);
+          }
+        }
+        return _results;
+      })();
+      curr = arr.length === 0 ? 0 : arr.length - 1;
+      _changeTag(curr);
+      return console.log(curr);
+    };
+    bindScroll = function() {
+      return $(window).scroll(detectScroll);
+    };
+    unbindScroll = function() {
+      return $(window).unbind('scroll');
+    };
+    _changeTag = function(index) {
+      $('nav li.selected').removeClass('selected');
+      return $('nav li').eq(index).addClass('selected');
+    };
+    return {
+      init: function() {
+        var that;
+        that = this;
+        $('nav li a').click(function() {
+          var index;
+          index = $(this).index('nav li a');
+          that.changeTag(index);
+          return that.jump(index);
+        });
+        return $(window).scroll(detectScroll);
+      },
+      changeTag: function(index) {
+        return _changeTag(index);
+      },
+      jump: function(index) {
+        var nextOffset;
+        unbindScroll();
+        nextOffset = $('h2').eq(index).position().top;
+        return $('body').animate({
+          scrollTop: nextOffset
+        }, 300, bindScroll);
+      }
+    };
+  })();
 }).call(this);
