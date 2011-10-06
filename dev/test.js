6a8ff3fa9b54c489bdf0907d47e3069935e0a243
Elliot Lai <ellliotlai@gmail.com>
2011-10-06 22:07:23 +0800

    argument interpolation with deferred commands

--- dev/test.js
+++ dev/test.js
@@ -1,57 +1,25 @@
-var redisev = require('../rediz'),
-    _ = require('underscore');
+var rediz = require('../rediz');
+var Queue = require('../lib/queue').Queue;
 
-var client = redisev.create();
+var client = rediz.create();
 client.connect(9838, 'filefish.redistogo.com', '0565140a7c5be78197e3c9f998083199');
 
-client.send('auth', '0565140a7c5be78197e3c9f998083199', function (err, res) {
-    console.log(res);
-});
 
 client
-    .send('set', 'a', 0)
-    .send('get', 'a')
+    .send('flushdb')
+    .send('set', 'cat:id', 0)
+    .send('get', 'cat:id')
+    .collect(function (res, basket) {
+        basket.id = res[2];
+    })
+    .defer()
+    .send('set', 'cat:#{id}', 'haha')
+    .send('keys', '*')
     .collect(function (res, basket) {
         console.log(res);
         console.log(basket);
-        basket.push(res[1]);
     })
-    .send('incr', 'a')
-    .send('ping')
-    .send('get', 'a')
-    .collect(function (res, basket) {
-        console.log(res);
-        console.log(basket);
-    });
-/*
-client
-    .ready(function () {
-        console.log('READY!');
-    })
-    .error(function (err) {
-        console.log(err);
-    });*/
-        
-      /*
-client.slave()
-    .send('SET', 'foo', 'bar')
-    .send('PING')
-    .send('PING')
-    .send('GET', 'foo')
-    .send('PING')
-    .send('PING')
-    .collect(function (res) {
-        console.log(res);
-    })
-    .send('SET', 'foo', 'fish')
-    .send('PING')
-    .send('GET', 'foo')
-    .send('PING')
-    .collect(function (res) {
-        console.log(res);
-    })
-    .send('GET', 'foo')
-    .send('PING')
-    .collect(function (res) {
-        console.log(res);
-    });*/
+    .disconnect();
+    
+    
+
--- rediz.js
+++ rediz.js
@@ -1,16 +1,9 @@
-var Emitter = require('events').EventEmitter,
-    util = require('util'),
-    _ = require('underscore'),
-    Redis = require('./lib/redis').Redis,
-    Queue = require('./lib/queue').Queue,
-    Collector = require('./lib/collector').Collector;
-
-// the missing _.init()
-_.init = function(array, n, guard) {
-    return (n != null) && !guard ? Array.prototype.slice.call(array, 0, n) : Array.prototype.slice.call(array, 0, -1);
-};
-
-
+var Emitter                     = require('events').EventEmitter,
+    util                        = require('util'),
+    Redis                       = require('./lib/redis').Redis,
+    Queue                       = require('./lib/queue').Queue,
+    Collector                   = require('./lib/collector').Collector;
+    Postie                      = require('./lib/postie').Postie;
 
 
 // ********************
@@ -18,79 +11,123 @@ _.init = function(array, n, guard) {
 // ********************
 
 var Client = function () {
-    var self            = this;
-    this._emitter       = new Emitter();                // event emitter
-    this._redis         = new Redis(this._emitter);     // redis socket
-    this._queue         = new Queue();                  // message queue
-    this._collector     = new Collector();              // collector
+    var self                    = this;
+    this._emitter               = new Emitter();                // event emitter
+    this._replyQueue            = new Queue();                  // reply queue
+    this._redis                 = new Redis(this._emitter);     // redis socket
+    this._collector             = new Collector(this);          // collector
+    this._deferred              = false;
+     
     
-    this._collector.newTask();
     
-    this._emitter.on('connect', function () {
-        console.log('connected');
-    });
-    this._emitter.on('error', function (err) {
-        console.log(err);
-    });
-    
-    this._emitter.on('redis reply', function (res) {
-        // Dequeue
-        var callback = self._queue.shift();
-        
-        // Redis Error
-        if (res instanceof Error)
-            callback(res, undefined);
-        else
-            callback(undefined, res);
-    });
+    this._emitter
+        .on('redis reply', function (res) {
+            // Dequeue
+            var callback = self._replyQueue.shift();
+                        
+            // Redis Error
+            if (res instanceof Error)
+                callback(res, undefined);
+            else
+                callback(undefined, res);
+        });
 };
 
+Client.prototype.on = function (event, callback) {
+    this._emitter.on(event, callback);
+};
 
 Client.prototype.connect = function (port, host, auth) {    
     this._redis.createConnection(
         port || 6379,
         host || '127.0.0.1'
     );
+    if (auth !== undefined)
+        this.send('AUTH', auth, function () {});
+    return this;
+};
+
+Client.prototype.disconnect = function () {
+    var self = this;
+    
+    this.defer()
+        .send('QUIT')
+        .collect(function () { 
+            // socket disconnect after 'QUIT' sent
+            self._redis.disconnect();
+        });
+    return this;
+};
+
+// takes a arguments object, parse it into stringified command, arguments and a callback function
+Client.prototype._commandParser = function (argument) {               
+    var hasCallback = (typeof argument[argument.length - 1] === 'function') ? true : false,
+        args = [];
+        
+    if (hasCallback) {    
+        // arguments to array, last element riiped (callback)
+        for (var i = 0, len = argument.length - 1; i < len; i++)
+            args[i] = argument[i].toString();            
+        return {
+            command: args,
+            callback: argument[argument.length - 1]
+        }        
+    } else {        
+        // arguments to array
+        for (var i = 0, len = argument.length; i < len; i++)
+            args[i] = argument[i].toString();            
+        return {
+            command: args,
+            callback: undefined
+        }
+    }    
 };
 
 Client.prototype.send = function () {
-                            
-    var hasCallback = _(arguments).chain()
-                        .last()
-                        .isFunction()
-                        .value(),
-        args,
-        callback;
-        
-    if (hasCallback) {
+
+    // digest the arguments
+    var arg = this._commandParser(arguments);
     
-        var init = _.init(arguments);   //ugly  
-        
-        args = _(init).map(function (elem) {
-            return elem.toString();
-        });
-        callback = _(arguments).last();
-        
-        this._queue.push(callback);
-        
-    } else {
+    if (arg.callback === undefined) { // no callback
+        if (this._deferred) // deferred
+            this._collector.defer(arg.command);
+        else 
+            this._send(arg.command, this._collector.assign());           
     
-        args = _(arguments).map(function (elem) {
-            return elem.toString();
-        });
-        
-        this._queue.push(this._collector.assign());
-        
-    }
+    } else { // sent it now!
     
-    this._redis.write.apply(null, args);
+        // reply queue
+        this._send(arg.command, arg.callback);
+    }   
     
+    // chaining
     return this;
 }
 
+Client.prototype.test = function (arg) {
+    console.log(arg);
+};
+Client.prototype._send = function (command, callback) {
+
+    // reply queue
+    this._replyQueue.push(callback);
+    // write to the redis socket
+    this._redis.write.apply(null, command);        
+    
+    // chaining
+    return this;
+};
+
+
+Client.prototype.defer = function () {
+    this._deferred = true;
+    return this;
+};
+
 Client.prototype.collect = function (callback) {
-    this._collector.done(callback);    
+    this._collector.callback(callback);
     this._collector.newTask();
+    this._deferred = false;
     return this;
 }
 
@@ -102,5 +139,3 @@ Client.prototype.collect = function (callback) {
 module.exports.create = function () {
     return new Client();
 }
-
-module.exports.Client = Client;
