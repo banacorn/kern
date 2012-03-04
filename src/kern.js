2b37dd5a1bc00fbde7d81fb05ad81083e08b8a13
Elliot Lai <ellliotlai@gmail.com>
2012-03-04 12:18:46 +0800

    this & that

--- src/kern.js
+++ src/kern.js
@@ -62,11 +62,11 @@ Kern.prototype.connect = function (port, host) {
     
     
     this.on = function (ev, cb) {
-        this._socket.on(ev, cb);
+        that._socket.on(ev, cb);
     };    
     
     this.end = function () {
-        this._socket.end();
+        that._socket.end();
     }
     
     return this;
